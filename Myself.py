1d17b2bfab574503cb0190c16cf722e08a7b3646
Marco Fajardo <77114515+HunholySaiyan@users.noreply.github.com>
2022-04-21 11:59:35 -0700

    Session 1 Prompt for Coding Journal 1

---
+++ Myself.py
@@ -0,0 +1,14 @@
+print ('\n Marco fajardo\n age:22\n pronouns:He/Him')
+
+print ('\n Favorite Movie: My favorite movie since I was a kid is Childs play ' , 
+		 ' \n I always enjoyed the doll and thought it as something funny instead of scary')
+
+print ('\n Favorite Food: My favorite food is Chicken Salad, I do not have a special reason' 
+		'\n why but since I was a kid its been the food I beg my mom to cook')
+
+
+
+
+
+	
+
